638af6483cfd0a3633551e646aa58ec8415630f1
Brandon Saint-John <saint-john@lbl.gov>
2026-07-10 15:27:46 -0700

    chore: initial commit
    Signed-off-by: Brandon Saint-John <saint-john@lbl.gov>

---
+++ src/Hdlbits/GettingStarted/StepOne.hs
@@ -0,0 +1,29 @@
+module Hdlbits.GettingStarted.StepOne where
+
+import Clash.Prelude
+
+-- | HDLBits problem /Step one/ (<https://hdlbits.01xz.net/wiki/Step_one>):
+-- a circuit with no inputs and one output that always drives 1.
+--
+-- 'high' is Clash's constant-1 'Bit', the equivalent of Verilog's @1'b1@.
+-- Modelled with 'Bool' instead, the output would simply be 'True'.
+--
+-- >>> one
+-- 1
+one :: Bit
+one = high
+
+-- | The synthesizable design: no input ports, one output port stuck at 1.
+topEntity :: Bit
+topEntity = one
+
+-- Name the generated entity and its output port after the HDLBits problem.
+{-# ANN topEntity
+  (Synthesize
+    { t_name = "step_one"
+    , t_inputs = []
+    , t_output = PortName "one"
+    }) #-}
+
+-- Make sure GHC does not apply any optimizations to the boundaries of the design.
+{-# OPAQUE topEntity #-}
